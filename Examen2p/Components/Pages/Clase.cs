4f6e85cd8cbbc2c49d3539b97398b773c6acea80
IssacPreciado <ipreciado3@ucol.mx>
2024-04-17 13:18:43 -0600

    examen

---
+++ Examen2p/Components/Pages/Clase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Producto
+{
+    [Required(ErrorMessage = "El número de inventario no debe estar vacío")]
+    [Range(1, int.MaxValue, ErrorMessage = "Debe ser un número entero positivo y mayor que 0")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El nombre no debe estar vacío")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "Debes seleccionar un departamento")]
+    public string? Departamentos { get; set; }
+}
